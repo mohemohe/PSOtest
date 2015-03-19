8b8a3225c41fc890751f94c559f13c422c841f7f
mohemohe <mohemohe@ghippos.net>
2015-03-19 19:46:45 +0900

    no message

---
+++ PSOtest/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSOtest
+{
+    class Program
+    {
+        class Particle
+        {
+            Position position;
+            Velocity velocity;
+        }
+        
+        const double _maxSpeed = 2.0;
+        const double _minSpeed = -maxSpeed;
+        const int _maxLoop = 100;
+        const double _moment = 0.8;
+
+        const double stageWidth = 10.0;
+        const double stageHeight = 10.0;
+
+        struct Position
+        {
+            public double x;
+            public double y;
+
+            public Position(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            } 
+        }
+
+        struct Velocity
+        {
+            public double x;
+            public double y;
+
+            public Position(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
+            } 
+        }
+
+        static void Main(string[] args)
+        {
+            // init
+            var particleList = new List<Particle>();
+            for (var i = 0; i < 100; i++)
+            {
+                particleList.Add(InitParticle);
+            }
+
+
+        }
+
+        static void PSO()
+        {
+            
+        }
+
+        static Particle InitParticle()
+        {
+            var rand = new Random();
+            Position position = Position{x = };
+        }
+
+        static void updatePosition(ref Position position, Velocity velocity)
+        {
+            pos.x += velocity.x;
+            pos.y += velocity.y;
+        }
+
+        static void updateVelocity(ref Velocity velocity, Position currentPosition, Position localBestPosition, Position globalBestPosition)
+        {
+            var rand = new Random();
+            velocity.x = _moment * velocity + 
+                          2 * rand.NextDouble() * (localBestPosition.x - currentPosition.x) + 
+                          2 * rand.NextDouble() * (globalBestPosition.x - currentPosition.x);
+            velocity.y = _moment * velocity +
+                          2 * rand.NextDouble() * (localBestPosition.y - currentPosition.y) +
+                          2 * rand.NextDouble() * (globalBestPosition.y - currentPosition.y);
+        }
+    }
+}
