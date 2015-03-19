6da7312b2611481a6f889cff21cd3044b4507bf5
mohemohe <mohemohe@ghippos.net>
2015-03-19 20:39:21 +0900

    no message

--- PSOtest/Program.cs
+++ PSOtest/Program.cs
@@ -10,25 +10,19 @@ namespace PSOtest
     {
         class Particle
         {
-            Position position;
-            Velocity velocity;
+            public Position currentPosition;
+            public Position personalBestPosition;
+            public Velocity velocity;
 
             public Particle(Position position, Velocity velocity)
             {
-                this.position = position;
+                this.currentPosition = position;
+                this.personalBestPosition = position;
                 this.velocity = velocity;
             }
         }
-        
-        const double _maxSpeed = 2.0;
-        const double _minSpeed = -maxSpeed;
-        const int _maxLoop = 100;
-        const double _moment = 0.8;
 
-        const double stageWidth = 10.0;
-        const double stageHeight = 10.0;
-
-        struct Position
+        class Position
         {
             public double x;
             public double y;
@@ -37,10 +31,10 @@ namespace PSOtest
             {
                 this.x = x;
                 this.y = y;
-            } 
+            }
         }
 
-        struct Velocity
+        class Velocity
         {
             public double x;
             public double y;
@@ -49,16 +43,36 @@ namespace PSOtest
             {
                 this.x = x;
                 this.y = y;
-            } 
+            }
         }
+
+        const double _maxSpeed = 2.0;
+        const double _minSpeed = -_maxSpeed;
+        const int _maxLoop = 100;
+        const double _moment = 0.8;
+
+        const double _stageWidth = 10.0;
+        const double _stageHeight = 10.0;
+
+        static Position _globalBestPosition;
 
         static void Main(string[] args)
         {
-            // init
+            // 初期化
             var particleList = new List<Particle>();
             for (var i = 0; i < 100; i++)
             {
                 particleList.Add(InitParticle());
+            }
+
+            // 最初の全体のベスポジを設定
+            _globalBestPosition = particleList[0].currentPosition;
+            foreach (var particle in particleList)
+            {
+                if (CalcFitness(particle.currentPosition) < CalcFitness(_globalBestPosition))
+                {
+                    _globalBestPosition = particle.currentPosition;
+                }
             }
 
 
@@ -69,35 +83,40 @@ namespace PSOtest
             
         }
 
-        static Particle InitParticle()
+        static Particle InitializeParticle()
         {
             var rand = new Random();
-            Position position = Position(
-                rand.Next(stageWidth * 100) / 100 * 2 - stageWidth / 2,
-                rand.Next(stageHeight * 100) / 100 * 2 - stageHeight / 2
+            Position position = new Position(
+                rand.Next((int)_stageWidth * 100) / 100 * 2 - _stageWidth / 2,
+                rand.Next((int)_stageHeight * 100) / 100 * 2 - _stageHeight / 2
             );
-            Velocity velocity = Velocity(
+            Velocity velocity = new Velocity(
                 rand.NextDouble() * 2,
                 rand.NextDouble() * 2
             );
 
-            return new Particle{position = position, velocity = velocity};
+            return new Particle(position, velocity);
+        }
+
+        static double CalcFitness(Position position)
+        {
+            return Math.Pow(position.x + position.y, 2);
         }
 
         static void updatePosition(ref Position position, Velocity velocity)
         {
-            pos.x += velocity.x;
-            pos.y += velocity.y;
+            position.x += velocity.x;
+            position.y += velocity.y;
         }
 
-        static void updateVelocity(ref Velocity velocity, Position currentPosition, Position localBestPosition, Position globalBestPosition)
+        static void updateVelocity(ref Velocity velocity, Position currentPosition, Position personalBestPosition, Position globalBestPosition)
         {
             var rand = new Random();
-            velocity.x = _moment * velocity + 
-                          2 * rand.NextDouble() * (localBestPosition.x - currentPosition.x) + 
+            velocity.x = _moment * velocity.x + 
+                          2 * rand.NextDouble() * (personalBestPosition.x - currentPosition.x) + 
                           2 * rand.NextDouble() * (globalBestPosition.x - currentPosition.x);
-            velocity.y = _moment * velocity +
-                          2 * rand.NextDouble() * (localBestPosition.y - currentPosition.y) +
+            velocity.y = _moment * velocity.y +
+                          2 * rand.NextDouble() * (personalBestPosition.y - currentPosition.y) +
                           2 * rand.NextDouble() * (globalBestPosition.y - currentPosition.y);
         }
     }
